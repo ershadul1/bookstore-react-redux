c523133b3509e281c0dc4151b8968e9cd556ec94
ershadul1 <ershadul.rayhan@gmail.com>
2021-01-26 13:57:53 +0600

    Add CategoryFIlter component

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import categories from '../constants';
+
+const CategoryFilter = ({ filter }) => (
+  <select onChange={filter}>
+    <option value="">All</option>
+    {categories.map(item => (
+      <option key={item} value={item}>
+        {item}
+      </option>
+    ))}
+  </select>
+);
+
+CategoryFilter.propTypes = {
+  filter: PropTypes.func.isRequired,
+};
+export default CategoryFilter;
