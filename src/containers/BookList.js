a0ad1e865f841318c7aaf45a5131a28282f56284
ershadul1 <ershadul.rayhan@gmail.com>
2021-01-26 13:58:30 +0600

    Add book filter functionalities in BookList

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,15 +1,30 @@
 import { connect } from 'react-redux';
-import { REMOVE_BOOK } from '../actions';
+import { REMOVE_BOOK, CHANGE_FILTER } from '../actions';
 import Book from '../components/Book';
+import CategoryFilter from '../components/CategoryFilter';
 
 const BookList = state => {
-  const books = state.allBooks;
+  const filterBooks = () => {
+    if (state.filter === '') {
+      return state.allBooks;
+    }
+    return state.allBooks.filter(book => book.bookCategory === state.filter);
+  };
+
   const handleRemoveBook = bookId => {
     state.removeBook(bookId);
   };
+
+  const handleFilterChange = e => {
+    state.changeFilter(e.target.value);
+  };
+
+  const books = filterBooks();
+
   return (
     <>
       <p>BookList</p>
+      <CategoryFilter filter={handleFilterChange} />
       <table>
         <thead>
           <tr>
@@ -20,26 +35,28 @@ const BookList = state => {
           </tr>
         </thead>
         <tbody>
-          {
-        books.map(item => (
-          <Book
-            key={item.bookId}
-            bookId={item.bookId}
-            bookTitle={item.bookTitle}
-            bookCategory={item.bookCategory}
-            remove={handleRemoveBook}
-          />
-        ))
-        }
+          {books.map(item => (
+            <Book
+              key={item.bookId}
+              bookId={item.bookId}
+              bookTitle={item.bookTitle}
+              bookCategory={item.bookCategory}
+              remove={handleRemoveBook}
+            />
+          ))}
         </tbody>
       </table>
     </>
   );
 };
 
-const mapStateToProps = state => ({ allBooks: state.allBooks });
+const mapStateToProps = state => ({
+  allBooks: state.allBooks,
+  filter: state.filter,
+});
 
 const mapDispatchToProps = dispatch => ({
   removeBook: bookId => dispatch(REMOVE_BOOK(bookId)),
+  changeFilter: category => dispatch(CHANGE_FILTER(category)),
 });
 export default connect(mapStateToProps, mapDispatchToProps)(BookList);
